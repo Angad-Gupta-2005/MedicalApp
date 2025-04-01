8410cfdc83fd20c986a01ef27e6676578ac41f09
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2025-04-01 13:04:45 +0530

    Profile UI is done

--- app/src/main/java/com/angad/medicalapp/screens/ProfileScreen.kt
+++ app/src/main/java/com/angad/medicalapp/screens/ProfileScreen.kt
@@ -1,14 +1,27 @@
 package com.angad.medicalapp.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -33,6 +46,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -70,7 +84,7 @@ fun ProfileScreen(
         }
 
         state.value.data != null -> {
-            Toast.makeText(context, "Data fetch successfully", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(context, "Data fetch successfully", Toast.LENGTH_SHORT).show()
             val data = state.value.data!!
 
             var showLogoutDialog by remember { mutableStateOf(false) }
@@ -116,14 +130,131 @@ fun ProfileScreen(
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
+                        .background(Color(0xFFE3F2FD))
                         .padding(innerPadding)
                 ) {
-                    Text(text = "Name: ${data.name}")
-                    Text(text = "Email: ${data.email}")
-                    Text(text = "Phone: ${data.phone_number}")
-                    Text(text = "Address: ${data.address}")
-                    Text(text = "Pin code: ${data.pin_code}")
-                    Text(text = "Date of account creation: ${data.date_of_account_creation}")
+                    Box(
+                        Modifier.fillMaxWidth().padding(top = 15.dp),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Profile Icon",
+                            modifier = Modifier.size(150.dp)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(25.dp))
+
+//                    Text(text = "Name: ${data.name}")
+//                    Text(text = "Email: ${data.email}")
+//                    Text(text = "Phone: ${data.phone_number}")
+//                    Text(text = "Address: ${data.address}")
+//                    Text(text = "Pin code: ${data.pin_code}")
+//                    Text(text = "Date of account creation: ${data.date_of_account_creation}")
+
+                //    Profile name
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(imageVector = Icons.Outlined.Person, contentDescription = "null")
+                        Column(
+                            modifier = Modifier.padding(start = 20.dp)
+                        ) {
+                            Text(
+                                text = "Name",
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(text = data.name)
+                        }
+                    }
+                //    Email name
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(imageVector = Icons.Outlined.Email, contentDescription = "null")
+                        Column(
+                            modifier = Modifier.padding(start = 20.dp)
+                        ) {
+                            Text(
+                                text = "Email",
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(text = data.email)
+                        }
+                    }
+
+                //    Phone number
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(imageVector = Icons.Outlined.Phone, contentDescription = "null")
+                        Column(
+                            modifier = Modifier.padding(start = 20.dp)
+                        ) {
+                            Text(
+                                text = "Phone number",
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(text = data.phone_number)
+                        }
+                    }
+
+                //    Address
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(imageVector = Icons.Outlined.Home, contentDescription = "null")
+                        Column(
+                            modifier = Modifier.padding(start = 20.dp)
+                        ) {
+                            Text(
+                                text = "Address",
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(text = data.address)
+                        }
+                    }
+
+                //    Pin code
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(imageVector = Icons.Outlined.Info, contentDescription = "null")
+                        Column(
+                            modifier = Modifier.padding(start = 20.dp)
+                        ) {
+                            Text(
+                                text = "Pin code",
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(text = data.pin_code)
+                        }
+                    }
+
+                //    Date of account creation
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(imageVector = Icons.Outlined.DateRange, contentDescription = "null")
+                        Column(
+                            modifier = Modifier.padding(start = 20.dp)
+                        ) {
+                            Text(
+                                text = "Date of Account Creation",
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(text = data.date_of_account_creation)
+                        }
+                    }
+
+
                 }
             }
             // Logout Confirmation Dialog
